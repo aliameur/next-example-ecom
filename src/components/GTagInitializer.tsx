3fe41e0521978ca5c31a218f3dcdb75f06d97ea9
SwallowAI <alex@swallow.ai>
2025-05-12 00:09:00 +0000

    feat: create layout files

---
+++ src/components/GTagInitializer.tsx
@@ -0,0 +1,24 @@
+'use client';
+
+import { useEffect } from 'react';
+import { usePathname, useSearchParams } from 'next/navigation';
+import * as gtag from '../utils/gtag';
+
+interface GTagInitializerProps {
+  isProduction: boolean;
+  gaTrackingId: string | undefined;
+}
+
+export default function GTagInitializer({ isProduction, gaTrackingId }: GTagInitializerProps) {
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+
+  useEffect(() => {
+    if (isProduction && gaTrackingId) {
+      const url = pathname + (searchParams.toString() ? `?${searchParams.toString()}` : '');
+      gtag.pageview(url);
+    }
+  }, [pathname, searchParams, isProduction, gaTrackingId]);
+
+  return null;
+}
